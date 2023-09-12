772db04f2888b094593f6430ec6302153966d6bc
Victor Monje <monjev086@gmail.com>
2023-09-11 22:29:04 -0600

    feat: add operators basic & const_pi

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import driver.driver;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main extends JFrame {
     //<editor-fold defaultstate="collapsed" desc="Globals Variables">
@@ -22,8 +26,8 @@ public class Main extends JFrame {
     private JButton bnt_1;
     private JPanel panel_calculator;
     private JButton btn_sin;
-    private JButton cosButton;
-    private JButton πButton;
+    private JButton btn_cos;
+    private JButton btn_pi;
     private JButton btn_sqrt;
     private JButton btn_x10;
     private JButton btn_cls;
@@ -31,17 +35,77 @@ public class Main extends JFrame {
     private JButton btn_log;
     private JButton btn_parent_right;
     private JButton btn_parent_left;
+    private JLabel lb_result;
+    private JLabel lb_result_info;
+    private JPanel jpanel_result;
     //</editor-fold>
 
-    public Main(){
+
+    public Main() {
         setContentPane(root);
         setTitle("Practica II");
-        setSize(400, 400);
+        setSize(700, 500);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         txt_info.setEnabled(false);
 
         setVisible(true);
+
+        // Funcion para traer el texto del boton y ponerla en el txt
+        ActionListener buttonClickListener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JButton button = (JButton) e.getSource(); // Obtener el botón que genero el evento
+                String buttonText = button.getText(); // Obtener el texto del botón
+                if (buttonText.equals("Sin") || buttonText.equals("Cos") || buttonText.equals("√")) {
+                    txt_info.setText(txt_info.getText() + buttonText + "("); // Agregar el texto al campo txt_info
+                } else if (buttonText.equals("x10")) {
+                    txt_info.setText(txt_info.getText()+buttonText+"^(");
+                } else {
+                    txt_info.setText(txt_info.getText() + buttonText); // Agregar el texto al campo txt_info
+                }
+            }
+        };
+        // Asignamos el mismo ActionListener a todos los botones numéricos
+        btn_0.addActionListener(buttonClickListener);
+        bnt_1.addActionListener(buttonClickListener);
+        btn_2.addActionListener(buttonClickListener);
+        btn_3.addActionListener(buttonClickListener);
+        btn_4.addActionListener(buttonClickListener);
+        btn_5.addActionListener(buttonClickListener);
+        btn_6.addActionListener(buttonClickListener);
+        btn_7.addActionListener(buttonClickListener);
+        btn_8.addActionListener(buttonClickListener);
+        btn_9.addActionListener(buttonClickListener);
+        btn_dot.addActionListener(buttonClickListener);
+        btn_pi.addActionListener(buttonClickListener);
+        btn_plus.addActionListener(buttonClickListener);
+        btn_minus.addActionListener(buttonClickListener);
+        btn_div.addActionListener(buttonClickListener);
+        btn_times.addActionListener(buttonClickListener);
+        btn_sin.addActionListener(buttonClickListener);
+        btn_cos.addActionListener(buttonClickListener);
+        btn_sqrt.addActionListener(buttonClickListener);
+        btn_cls.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txt_info.setText("");
+            }
+        });
+
+        btn_x10.addActionListener(buttonClickListener);
+        btn_log.addActionListener(buttonClickListener);
+        btn_parent_right.addActionListener(buttonClickListener);
+        btn_parent_left.addActionListener(buttonClickListener);
+
+        btn_equals.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lb_result.setText(String.valueOf(driver.calculateExpression(txt_info.getText())));
+            }
+        });
+
+
     }
 
     public static void main(String[] args) {
--- src/driver/driver.java
+++ src/driver/driver.java
@@ -1,5 +1,71 @@
 package driver;
 
+import java.util.Stack;
+
 public class driver {
 
+    public static double calculate_expression(String exp) {
+        exp = exp.replaceAll("π", String.valueOf(Math.PI));
+        return evalExp(exp);
+    }
+
+    public static double evalExp(String exp) {
+        Stack<Double> numbers = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+
+        for (int i = 0; i < exp.length(); i++) {
+            char c = exp.charAt(i);
+            if (Character.isDigit(c)) {
+                StringBuilder num = new StringBuilder();
+                while (i < exp.length() && (Character.isDigit(exp.charAt(i)) || exp.charAt(i) == '.')) {
+                    num.append(exp.charAt(i));
+                    i++;
+                }
+                i--;
+
+                numbers.push(Double.parseDouble(num.toString()));
+            } else if (c == '(') {
+                operators.push(c);
+            } else if (c == ')') {
+                while (operators.peek() != '(') {
+                    choose_operation(numbers, operators);
+                }
+                operators.pop();
+            } else if (c == '+' || c == '-' || c == 'x' || c == '/') {
+                while (!operators.isEmpty() && priority(operators.peek()) >= priority(c)) {
+                    choose_operation(numbers, operators);
+                }
+                operators.push(c);
+            }
+        }
+
+        while (!operators.isEmpty()) {
+            choose_operation(numbers, operators);
+        }
+
+        return numbers.pop();
+    }
+
+    private static int priority(char operator) {
+        if (operator == '+' || operator == '-') {
+            return 1;
+        } else if (operator == 'x' || operator == '/') {
+            return 2;
+        }
+        return 0;
+    }
+
+    private static void choose_operation(Stack<Double> numbers, Stack<Character> operators) {
+        char operator = operators.pop();
+        double num2 = numbers.pop();
+        double num1 = numbers.pop();
+        double result = switch (operator) {
+            case '+' -> num1 + num2;
+            case '-' -> num1 - num2;
+            case 'x' -> num1 * num2;
+            case '/' -> num1 / num2;
+            default -> 0;
+        };
+        numbers.push(result);
+    }
 }
